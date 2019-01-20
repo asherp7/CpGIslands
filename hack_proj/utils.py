de7409c54e0b61bcf4cff06bc54eae21f8977e5c
Lior Zfira <lior@rsip.co.il>
2019-01-20 22:14:37 +0200

    Add classifier and display functions

--- hack_proj/utils.py
+++ hack_proj/utils.py
@@ -1,14 +1,20 @@
 import csv
 import bisect
 import numpy as np
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc, classification_report
 import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+import collections
 
+import DataReader
 
 RANGE_START = 'start'
 RANGE_END = 'end'
 RANGE_LEN = 'length'
 RANGE_SCORE = 'score'
+
+ALL_BASES = ['A', 'C', 'G', 'T']
+UNKNOWN_BASE = 'N'
 
 
 def read_range(fd, start, length):
@@ -160,6 +166,38 @@ class GenomeTagRange(object):
             return curr_island_tag
 
 
+class DataExtractor(object):
+    def __init__(self, data_reader, genome_tag_reader):
+        self.data_reader = data_reader  # type: DataReader.DataReader
+        self.genome_tag_reader = genome_tag_reader  # type: GenomeTagRange
+
+    def get_base_tagged_seq(self, chr, seq_num=1, pad_size=1000, with_unknown=False):
+        all_seq = []
+        all_labels = []
+        all_ranges = self.genome_tag_reader.get_tagged_ranges(chr)
+        for i in range(min(seq_num, len(all_ranges[RANGE_START]))):
+            curr_start = all_ranges[RANGE_START][i] - pad_size
+            curr_end = all_ranges[RANGE_END][i] + pad_size
+
+            curr_labels = self.genome_tag_reader.get_tag_for_range(chr, curr_start, curr_end - curr_start)
+            curr_seq = self.data_reader.get_sequence('hg19.fa.chr1', curr_start, curr_end - curr_start)
+
+            if not curr_seq.find(UNKNOWN_BASE):
+                all_seq.append(curr_seq)
+                all_labels.append(curr_labels)
+
+        return all_seq, all_labels
+
+
+def get_results_from_dict_list(dict_list, label_key='label', pred_key='score'):
+    y = []
+    y_pred = []
+    for d in dict_list:
+        y.append(d[label_key])
+        y_pred.append(d[pred_key])
+    return y, y_pred
+
+
 def show_roc_curve(y, y_score, title='ROC Curve'):
     fpr, tpr, thresholds = roc_curve(y, y_score)
     roc_auc = auc(fpr, tpr)
@@ -183,3 +221,110 @@ def show_roc_curve(y, y_score, title='ROC Curve'):
     ax2.set_ylim([thresholds[-1], thresholds[0]])
     ax2.set_xlim([fpr[0], fpr[-1]])
     plt.show()
+
+
+def get_prediction_stats(y, y_pred, target_names=('Regular', 'CPG')):
+    return classification_report(y, y_pred, target_names=target_names)
+
+
+def print_prediction_stats(y, y_pred, target_names=('Regular', 'CPG')):
+    print(get_prediction_stats(y, y_pred, target_names))
+
+
+def get_seq_windows(seq, window_size, base_labels=None):
+    all_seq = []
+    all_labels = []
+    side = int(window_size / 2)
+    for i in range(side, len(seq) - side):
+        all_seq.append(seq[i-side:i+side])
+        if base_labels is not None:
+            all_labels.append(base_labels[i-side:i+side])
+    new_seq = seq[side:len(seq) - side]
+    if base_labels is None:
+        return new_seq, all_seq
+    else:
+        new_lables = base_labels[side:len(seq) - side]
+        return new_seq, all_seq, new_lables, all_labels
+
+
+class IslandClassifier(object):
+    def __init__(self, threshold):
+        self.threshold = threshold
+
+    def set_threshold(self, threshold):
+        self.threshold = threshold
+
+    def get_seq_prob(self, all_seq):
+        pass
+
+    def get_seq_classification(self, all_seq):
+        pass
+
+
+class CGFreqClassifier(IslandClassifier):
+    def __init__(self, seq_window_len, cg_threshold=None):
+        IslandClassifier.__init__(self, cg_threshold)
+        self.seq_window_len = seq_window_len
+        self.svc_clf = None  # type: SVC
+
+    def set_threshold(self, threshold):
+        self.threshold = threshold
+
+    def get_seq_counts(self, seq):
+        count_dict = {}
+        for c in ALL_BASES:
+            count_dict[c] = 0
+
+        for i in range(len(seq)):
+            try:
+                count_dict[seq[i]] += 1
+            except:
+                continue
+        return count_dict
+
+    def get_data_from_seq_list(self, all_seq):
+        x = []
+        for i in range(len(all_seq)):
+            seq = all_seq[i]
+            count_dict = self.get_seq_counts(seq)
+            x.append(((count_dict.get('G', 0) + count_dict.get('C', 0)) / len(seq)))
+
+        x = np.reshape(np.array(x), (-1, 1))
+        return x
+
+    def fit(self, all_seq, y):
+        x = []
+        new_y = []
+        print('Create data to fit...')
+        for i in range(len(all_seq)):
+            seq = all_seq[i]
+            new_seq, all_windows_seq = get_seq_windows(seq, self.seq_window_len)
+            idx = np.random.choice(len(all_windows_seq), max(int(len(all_windows_seq) * 0.01), 1), replace=False)
+            all_windows_seq = [all_windows_seq[j] for j in idx]
+            curr_x = self.get_data_from_seq_list(all_windows_seq)
+            # curr_x = np.random.choice(np.squeeze(curr_x), max(int(curr_x.size * 0.01), 1), replace=False)
+            for val in curr_x:
+                x.append(val)
+                new_y.append(y[i])
+            # for win_seq in all_windows_seq:
+            #     count_dict = self.get_seq_counts(win_seq)
+            #     x.append(((count_dict.get('G', 0) + count_dict.get('C', 0)) / len(win_seq)))
+            #     new_y.append(y[i])
+
+        x = np.reshape(np.array(x), (-1, 1))
+        self.svc_clf = SVC(kernel='linear', probability=True, max_iter=10000)
+        print('Fit data...')
+        self.svc_clf.fit(np.array(x), np.array(new_y))
+
+    def get_seq_prob(self, all_seq):
+        x = self.get_data_from_seq_list(all_seq)
+        y_pred = self.svc_clf.predict_proba(x)
+        return y_pred[:, 1]
+
+    def classify_seq(self, seq, labels=None):
+        new_seq, all_seq, new_lables, all_labels = get_seq_windows(seq, window_size=self.seq_window_len,
+                                                                   base_labels=labels)
+        y_prob = self.get_seq_prob(all_seq)
+        y_pred = np.zeros_like(y_prob)
+        y_pred[y_prob >= self.threshold] = 1
+        return y_pred
