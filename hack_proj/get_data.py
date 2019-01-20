6a29af0a86cdce590c325ff55bfb5ab51287965c
Dana-Sender <dana.sender1@mail.huji.ac.il>
2019-01-20 19:06:17 +0200

    update

--- hack_proj/get_data.py
+++ hack_proj/get_data.py
@@ -1,11 +1,14 @@
+import os
+import pickle
 from utils import *
 import sys
 
+NUM_OF_CHROM = 6
 THRESHOLD = 500
 SEQ_LEN = 1001
 
 
-def get_locations(chrom_name, genome_tag_range):
+def get_locations(genome_tag_range, chrom_name):
     """
     :return: Two lists of lists with the location of cpg island and not
     """
@@ -36,37 +39,52 @@ def get_chrom(chrom_name):
 
 def set_data(chrom_name, chrom_string, loc_cpg_island, loc_not_cpg_island, genome_tag_range):
     """
-    :param chrom: the wanted chromosome as string
-    :param loc_cpg_island: list of lists with the start point and end point of all the cpgisland
-    :param loc_not_cpg_island: list of lists with the start point and end point of all the not cpg island
-    :return: two lists: a list of the seqs and a list of their labels (accordingly)
-    """
-    seqs_list = list()
-    labels_list = list()
-    nucleotids_labels = list()
+    # :param chrom_name: the name of the wanted chorom
+    # :param chrom_string: the wanted chromosome as string
+    # :param loc_cpg_island: locations of the cpg islands
+    # :param loc_not_cpg_island: locations of the not cpg islands
+    # :param genome_tag_range: object of the class utils
+    # :return: for each chromosom, a tuple of three which include : the Str of the seq, his label, and its nucleotide
+        label for each letter
+    # """
+
+    tuple_of_data = list()
     for not_cpg in loc_not_cpg_island:
-        seqs_list.append(chrom_string[not_cpg[0]:not_cpg[1]].upper())
-        labels_list.append(0)
+        a = chrom_string[not_cpg[0]:not_cpg[1]].upper()
+        b = "0"
         start, length = not_cpg[0], not_cpg[1] - not_cpg[0]
-        nucleotids_labels.append(genome_tag_range.get_tag_for_range(chrom_name, start, length))
+        c = genome_tag_range.get_tag_for_range(chrom_name, start, length)
+        tuple_of_data.append((a, b, c))
     for cpg in loc_cpg_island:
         if cpg[1] - cpg[0] < THRESHOLD:
             continue
-        seqs_list.append(chrom_string[cpg[0]:cpg[0] + SEQ_LEN].upper())
-        labels_list.append(1)
+        a = chrom_string[cpg[0]:cpg[0] + SEQ_LEN].upper()
+        b = "1"
         start, length = cpg[0], cpg[1] - cpg[0]
-        nucleotids_labels.append(genome_tag_range.get_tag_for_range(chrom_name, start, length))
-
-    return seqs_list, labels_list, nucleotids_labels
+        c = genome_tag_range.get_tag_for_range(chrom_name, start, length)
+        tuple_of_data.append((a, b, c))
+    return tuple_of_data
 
 
 if __name__ == '__main__':
-    """ sys.argv[1] = the wanted chromosome's name 
+    """ sys.argv[1] = directory name for the split data folder
         sys.argv[2] = the location of the CGI.hg19.bed file
         sys.argv[3] = the location of the hg19.chrom.sizes file"""
-    chrom_name = sys.argv[1]
+
+    dir_name = sys.argv[1]
     genome_tag_range = GenomeTagRange(sys.argv[2], sys.argv[3])
-    loc_cpg_island, loc_not_cpg_island = get_locations(chrom_name, genome_tag_range)
-    chrom_string = get_chrom(chrom_name)
-    seqs_list, labels_list, nucleotids_labels = set_data(chrom_name, chrom_string, loc_cpg_island, loc_not_cpg_island, genome_tag_range)
+    all_data = list()
+
+    i = 0
+    for chrom_name in os.listdir(dir_name):
+        if chrom_name != "chr1" and i < NUM_OF_CHROM:
+            print(chrom_name)
+            chrom_string = get_chrom(chrom_name)
+            loc_cpg_island, loc_not_cpg_island = get_locations(genome_tag_range, chrom_name)
+            one_chrom = set_data(chrom_name, chrom_string, loc_cpg_island, loc_not_cpg_island, genome_tag_range)
+            all_data.extend(one_chrom)
+            i += 1
+
+    with open('all_data', 'wb') as f:
+        pickle.dump(all_data, f)
 
