45b1cf5de4ee993e16bfa507be27ad265c4f172e
Dana-Sender <dana.sender1@mail.huji.ac.il>
2019-01-20 15:41:54 +0200

    update

--- hack_proj/get_data.py
+++ hack_proj/get_data.py
@@ -2,14 +2,12 @@ from utils import *
 import sys
 
 THRESHOLD = 500
-
 SEQ_LEN = 1001
 
 
 def get_locations():
     """
-
-    :return: Two lists of lists with the location
+    :return: Two lists of lists with the location of cpg island and not
     """
     genome_tag_range = GenomeTagRange(
         r'C:\Users\User\Documents\Thirdyear\Tommy_course\CpGIslands\hack_proj\data\CGI.hg19.bed',
@@ -30,12 +28,22 @@ def get_locations():
 
 
 def get_chrom(chrom):
+    """
+    :param chrom: the wanted chromosome
+    :return: string represents the chromosome
+    """
     with open(chrom, 'r') as f:
         chrom = f.read()
     return chrom
 
 
 def set_data(chrom, loc_cpg_island, loc_not_cpg_island):
+    """
+    :param chrom: the wanted chromosome
+    :param loc_cpg_island: list of lists with the start point and end point of all the cpgisland
+    :param loc_not_cpg_island: list of lists with the start point and end point of all the not cpg island
+    :return: two lists: a list of the seqs and a list of their labels (accordingly)
+    """
     seqs_list = list()
     labels_list = list()
     for not_cpg in loc_not_cpg_island:
@@ -43,17 +51,16 @@ def set_data(chrom, loc_cpg_island, loc_not_cpg_island):
         labels_list.append(0)
 
     for cpg in loc_cpg_island:
-        if cpg[1]-cpg[0] < THRESHOLD:
+        if cpg[1] - cpg[0] < THRESHOLD:
             continue
-        seqs_list.append(chrom[cpg[0]:cpg[0]+SEQ_LEN].upper())
+        seqs_list.append(chrom[cpg[0]:cpg[0] + SEQ_LEN].upper())
         labels_list.append(1)
 
     return seqs_list, labels_list
 
 
-
 if __name__ == '__main__':
+    """ sys.argv[1] = the wanted chromosome """
     loc_cpg_island, loc_not_cpg_island = get_locations()
     chrom = get_chrom(sys.argv[1])
     seqs_list, labels_list = set_data(chrom, loc_cpg_island, loc_not_cpg_island)
-
